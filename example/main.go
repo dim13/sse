c62d547e854ed5bfc18f4f32b50d924b3566c906
Dimitri Sokolyuk <demon@dim13.org>
2021-02-26 12:58:16 +0100

    Embed template

--- example/main.go
+++ example/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"net/http"
 	"text/template"
@@ -9,28 +10,14 @@ import (
 	"github.com/dim13/sse"
 )
 
-const index = `<!DOCTYPE html>
-<html>
-<head>
-<style>
-	body { font-family: 'Go', sans-serif; }
-</style>
-<script>
-	const events = new EventSource("/events");
-	events.addEventListener("now", function(e) {
-		document.getElementById("now").innerHTML = e.data;
-	});
-</script>
-<body>
-	<div id="now"></div>
-</body>
-</html>`
+//go:embed index.tmpl
+var static embed.FS
 
 func main() {
 	e := sse.New("now", 60)
 	http.Handle("/events", e)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		template.Must(template.New("index").Parse(index)).Execute(w, nil)
+		template.Must(template.ParseFS(static, "index.tmpl")).Execute(w, nil)
 	})
 	go func() {
 		t := time.NewTicker(time.Second)
